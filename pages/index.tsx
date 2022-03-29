03067500c8c93aedd07dfe38714a1444c218909a
Evan Conrad <evan@roomservice.dev>
2022-03-29 09:41:26 -0700

    add strangemood

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
         </li>
       </ul>
 
-      <b className="mt-8 flex">People:</b>
+      <b className="mt-8 flex">Current folks:</b>
       <ul>
         <li>
           <a className="underline" href="https://twitter.com/evanjconrad">
@@ -41,6 +41,50 @@ const Home: NextPage = () => {
           <a className="underline" href="https://twitter.com/jacobpedd">
             Jacob Peddicord
           </a>
+        </li>
+      </ul>
+
+      <b className="mt-8 flex">Past experiments:</b>
+      <p className="text-gray-500 text-sm mb-4">
+        Rebase has tried several things in the past that didn't work out.
+      </p>
+      <ul className="list-disc pl-4">
+        <li className="mb-2">
+          <div>Buildrecall - A 412 CPU Rust build farm</div>
+          <div className="text-gray-500 text-sm">
+            Thought we could make it not cost $600 a month; nope.
+          </div>
+        </li>
+        <li className="mb-2">
+          <div>Streambus - Build your own YouTube</div>
+          <div className="text-gray-500 text-sm">
+            Thought we could make YouTubers more money; merch and sponsorships
+            made more than we thought.
+          </div>
+        </li>
+        <li className="mb-2">
+          <div>Refader - React for editing live video streams</div>
+          <div className="text-gray-500 text-sm">
+            This was really cool, I think we talked out of it too early to be
+            honest. Kevin King is a very smart person and you should work with
+            him.
+          </div>
+        </li>
+        <li className="mb-2">
+          <div>
+            Room Service - A plug-and-play CRDT-based multiplayer engine
+          </div>
+          <div className="text-gray-500 text-sm">
+            Most folks don't need CRDTs.
+          </div>
+        </li>
+
+        <li className="mb-2">
+          <div>Quirk - An open source mental health app</div>
+          <div className="text-gray-500 text-sm">
+            When people feel better, they stop paying for your product. If
+            people don't feel better, they stop paying for your product.
+          </div>
         </li>
       </ul>
     </div>
